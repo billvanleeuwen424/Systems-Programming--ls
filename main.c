b48da44f1c5f07e545ba0fb8fc9550ddcce8d5c1
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-01-27 19:01:33 -0500

    So far so good. Figuring out the directory stuff

--- main.c
+++ main.c
@@ -8,26 +8,34 @@ Objectives of this program:
 #include <stdio.h>
 #include <string.h>
 
-//for create directory
-#include <sys/stat.h>
 
-//for opendir
+//for opendir and readdir
 #include <dirent.h>
+
+
 //command line params
 //argc = num of params
 //char *argv[] is the array of params
 //int argc, char *argv[]
+
+
+
 int main(){
-    printf("Hello World!\n");
 
-    const char *dirpath = "./";
+    //directory path
+    const char *dirpath = "/home/billDesktop";
 
-    DIR *opendir(const char *dirpath);
+    //pointer to a dirent structure
+    struct dirent *directoryEntry;
 
-    mode_t mode = 04444;
-    const char *pathname = "./yeet.txt";
+    //pointer to the directory type
+    DIR *dir = opendir(dirpath);
 
-    int yes = mkdir(pathname, mode);
-    
-     return yes;
+    //reference: https://stackoverflow.com/questions/3554120/open-directory-using-c
+    while ((directoryEntry = readdir(dir)) != NULL){
+        printf ("%s\n", directoryEntry->d_name);
+    }
+
+     return 0;
+     
 }
