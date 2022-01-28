648eac3b139b30213340da1467dce14b68c1650a
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-01-27 20:01:39 -0500

    got current dir

--- main.c
+++ main.c
@@ -11,14 +11,17 @@ Objectives of this program:
 
 //for opendir and readdir
 #include <dirent.h>
-
+//file stats
+#include <sys/stat.h>
+//for getcwd
+#include <unistd.h>
 
 //command line params
 //argc = num of params
 //char *argv[] is the array of params
 //int argc, char *argv[]
 
-
+#define MAX_BUFFER 4096
 
 int main(){
 
@@ -31,11 +34,20 @@ int main(){
     //pointer to the directory type
     DIR *dir = opendir(dirpath);
 
+    //file stats
+    struct stat *fileStats;
+
     //reference: https://stackoverflow.com/questions/3554120/open-directory-using-c
     while ((directoryEntry = readdir(dir)) != NULL){
-        printf ("%s\n", directoryEntry->d_name);
+        printf("%s\n", directoryEntry->d_name);
+    
     }
 
-     return 0;
-     
+    //get the current dir
+    char *cwd = getcwd(cwd, MAX_BUFFER);
+
+    
+    //close the directory stream
+    return closedir(dir);
+
 }
