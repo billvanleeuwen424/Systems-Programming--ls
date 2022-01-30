9c22825c6b0d6afd6279e580617e57d284973008
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-01-30 13:30:34 -0500

    Got string concatenation to work

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ Objectives of this program:
     the most recently modified file
     the oldest file
 */
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -25,13 +27,12 @@ Objectives of this program:
 
 int main(){
 
-
-
-    //directory path
-    const char *dirpath = "/home/billDesktop";
-
     //get the current dir
     char *cwd = getcwd(cwd, MAX_BUFFER);
+
+    //directory path
+    char dirpath[] = "/home/billDesktop/Documents/TrentU/3380/";
+
 
     //pointer to a dirent structure
     struct dirent *directoryEntry;
@@ -40,21 +41,28 @@ int main(){
     DIR *dir = opendir(dirpath);
 
     //file stats
-    //struct stat fileStats;
-    //struct stat *pfileStats = &fileStats;
+    struct stat fileStats;
+    struct stat *pfileStats = &fileStats;
+
+
     //reference: https://stackoverflow.com/questions/3554120/open-directory-using-c
     while ((directoryEntry = readdir(dir)) != NULL){
-        //print directory
-        printf("%s\n", directoryEntry->d_name);
 
+        char filename[250];
+
+        strcpy(filename,dirpath);
+        const char *oldfilename = directoryEntry->d_name;
+
+
+        strcat(filename,oldfilename);
+        printf("%s\n", filename);
+
+        /*
         //get stats, print stats
-        //stat(dirpath, pfileStats);
-        //printf("%lu", pfileStats);
-
+        stat(filename, &fileStats);
+        printf("%i\n", pfileStats->st_atime);
+        */
     }
-
-    
-
     
     //close the directory stream
     return closedir(dir);
